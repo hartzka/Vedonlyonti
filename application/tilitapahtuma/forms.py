5ce07e867c05ca1dd79712d16d786db1adfed13c
Hartzell Kai <kaihartz@cubbli18-gold.local>
2018-12-15 12:14:13 +0200

    heroku11

--- application/tilitapahtuma/forms.py
+++ application/tilitapahtuma/forms.py
@@ -2,14 +2,14 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField, validators
 
 class TilisiirtoForm(FlaskForm):
-    tilisiirto = IntegerField("Summa", [validators.NumberRange(min = 0, message="Anna positiivinen luku")])
+    tilisiirto = IntegerField("Summa", [validators.NumberRange(min = 0, max = 10000, message="Anna positiivinen luku")])
     
     class Meta:
         csrf = False
 
 
 class PankkisiirtoForm(FlaskForm):
-    pankkisiirto = IntegerField("Summa", [validators.NumberRange(min = 0, message="Anna positiivinen luku")])
+    pankkisiirto = IntegerField("Summa", [validators.NumberRange(min = 0, max = 10000, message="Anna positiivinen luku")])
     
     class Meta:
         csrf = False
